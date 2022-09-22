2f2b7719c4ad909b9de75f636bd0846ffab001be
Ivan Hu <ivanhu2246@gmail.com>
2022-09-22 16:28:39 +0800

    Added TodoItem model

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        // Question mark means it is a nullable type. The string can be of any valid string value, plus an additional null value.
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
