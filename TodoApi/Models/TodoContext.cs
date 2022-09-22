8688447dc4fa460ac39ab8a5cacc3ec2f9e32b2a
Ivan Hu <ivanhu2246@gmail.com>
2022-09-22 16:52:16 +0800

    Added TodoContext

---
+++ TodoApi/Models/TodoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options)
+        { 
+        }
+
+        // null forgiving operator to the compiler: "this isn't null, trust me"
+        // prevents the compiler from issuing a warning for a possible null reference
+        public DbSet<TodoItem> TodoItems { get; set; } = null!;
+    }
+}
